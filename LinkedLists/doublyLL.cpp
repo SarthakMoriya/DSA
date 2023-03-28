5e420b888e383e5d6e12ebf3c672e6d36eb5756f
Sarthak30-03-2003 <sarthak8544@gmail.com>
2023-03-28 20:33:01 +0530

    doublyinsertDeletion

--- LinkedLists/doublyLL.cpp
+++ LinkedLists/doublyLL.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 class Node
 {
 public:
@@ -14,9 +15,11 @@ public:
         this->prev = NULL;
     }
 };
+void deleteAtHead(Node *&head);
 
 void forwardTraverse(Node *head)
 {
+    // cout<<head->data;
     if (head == NULL)
     {
         cout << "Empty LL!" << endl;
@@ -31,7 +34,7 @@ void forwardTraverse(Node *head)
     }
     cout << endl;
 }
-void backwardTraverse(Node *head)
+void backwardTraverse(Node *&head)
 {
     if (head == NULL)
     {
@@ -86,7 +89,7 @@ void insertAtTail(Node *&head, int data)
     node->prev = temp;
 }
 
-void insertByIndex(Node *head, int data, int index)
+void insertByIndex(Node *&head, int data, int index)
 {
     Node *node = new Node(data);
     int count = 0;
@@ -98,25 +101,118 @@ void insertByIndex(Node *head, int data, int index)
     }
 
     Node *temp = head;
-    Node *prev = NULL;
 
-    while (count < index - 1 && temp != NULL)
-    {
-        prev = temp;
-        temp = temp->next;
-    }
-
-    if (temp == NULL)
-    {
-        cout << "please provide a valid index !";
-        return;
-    }
-
-    if (prev == NULL)
+    if (index == 0)
     {
         insertAtHead(head, data);
         return;
     }
+
+    // 1---2---3 index=3
+    while (count < index - 1 && temp->next != NULL)
+    {
+        temp = temp->next;
+        count++;
+    }
+    if (temp->next == NULL)
+    {
+        insertAtTail(head, data);
+        return;
+    }
+
+    node->next = temp->next;
+    temp->next->prev = node;
+    temp->next = node;
+    node->prev = temp;
+}
+
+void deleteAtHead(Node *&head)
+{
+    if (head == NULL)
+    {
+        cout << "Can't perform deletion on Emplty LL";
+        return;
+    }
+    // cout << "reached HERE";
+    Node *temp = head;
+    // cout<<"\n"<<head->data;
+    if (head->next != NULL)
+    {
+        head = head->next;
+        head->prev = NULL;
+        temp->next = NULL;
+        delete temp;
+        return;
+    }
+    else
+    {
+        head = NULL;
+        return;
+    }
+}
+
+void deleteAtTail(Node *&head)
+{
+    if (head == NULL)
+    {
+        cout << "Can't perform deletion on Emplty LL";
+        return;
+    }
+
+    Node *temp = head;
+    Node *prev = NULL;
+
+    while (temp->next != NULL)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+    if (prev == NULL)
+    {
+        cout << "\nHead Deletion!" << endl;
+        deleteAtHead(head);
+        return;
+    }
+    prev->next = NULL;
+    temp->prev = NULL;
+    delete temp;
+}
+
+void deleteAtIndex(Node *&head, int index)
+{
+    if (index == 0)
+    {
+        deleteAtHead(head);
+        return;
+    }
+
+    if (head == NULL)
+    {
+        cout << "Head Deletion not Possible on emplty LL!";
+        return;
+    }
+
+    Node *prev = NULL;
+    Node *temp = head;
+    int count = 0;
+    while (count < index && temp != NULL)
+    {
+        prev = temp;
+        temp = temp->next;
+        count++;
+    }
+
+    if(temp == NULL){
+        cout<<"Node Deletion Not Possible please provide valid Index...!";
+        return;
+    }
+    temp->next->prev = temp->prev;
+    prev->next = temp->next;
+
+    cout << "\nNode to be deleted's data::" << temp->data<<endl;
+    temp->next = NULL;
+    temp->prev = NULL;
+    delete temp;
 }
 
 int main()
@@ -141,6 +237,31 @@ int main()
     cout << "After inserting at end  :: ";
     insertAtTail(head, 5);
     insertAtTail(head, 6);
+    forwardTraverse(head);
+
+    cout << "After inserting at Any Index  :: ";
+    insertByIndex(head, 7, 6);
+    insertByIndex(head, 0, 0);
+    forwardTraverse(head);
+
+    cout << "After Deleting At Head  :: ";
+    deleteAtHead(head);
+    deleteAtHead(head);
+    forwardTraverse(head);
+
+    // cout << "After Deleting At Tail  :: ";
+    // deleteAtTail(head);
+    // deleteAtTail(head);
+    // deleteAtTail(head);
+    // deleteAtTail(head);
+    // deleteAtTail(head);
+    // deleteAtTail(head);
+    // deleteAtTail(head); // Now head is  NULL at this line
+    // forwardTraverse(head);
+
+    cout << "After Deleting At Index  :: ";
+    deleteAtIndex(head,0);
+    deleteAtIndex(head,32);
     forwardTraverse(head);
     return 0;
 }
---
+++ LinkedLists/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+    // cout << "After Deleting At Tail  :: ";
+    // deleteAtTail(head);
+    // deleteAtTail(head);
+    // deleteAtTail(head);
+    // deleteAtTail(head);
+    // deleteAtTail(head);
+    // deleteAtTail(head);
+    // deleteAtTail(head); // Now head is  NULL at this line
+    // forwardTraverse(head);
