3567ca439c93aa5a7dbe6c8de7ef2df528f1906c
Sarthak30-03-2003 <sarthak8544@gmail.com>
2023-04-07 13:02:18 +0530

    d

--- LinkedLists/circularLL.cpp
+++ LinkedLists/circularLL.cpp
@@ -17,7 +17,7 @@ public:
 void traverse(Node *tail)
 {
     cout << "TRAVERSING..." << endl;
-    // cout << tail->data << " ";
+    // cout << tail->data th<< " ";
     if (tail == NULL)
     {
         cout << "empty LL!";
